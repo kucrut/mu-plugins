8a7ee97e81d93a85f01f9c02d8d1d2c651c6c674
Dzikri Aziz <kvcrvt@gmail.com>
2013-09-18 14:59:43 +0700

    Post List Table Thumbnail: Fix adding custom column callback

--- kc-post-list-table-thumbnails/kc-post-list-table-thumbnails.php
+++ kc-post-list-table-thumbnails/kc-post-list-table-thumbnails.php
@@ -22,13 +22,13 @@ final class KC_Post_List_Table_Thumbnail {
 		if ( !post_type_supports($screen->post_type, 'thumbnail') )
 			return false;
 
-		add_filter( "manage_{$screen->post_type}_posts_columns", array(__CLASS__, '_add_thumbnail_column'), 10, 2 );
+		add_filter( "manage_{$screen->post_type}_posts_columns",       array(__CLASS__, '_add_thumbnail_column') );
 		add_action( "manage_{$screen->post_type}_posts_custom_column", array(__CLASS__, '_display_thumbnail_column'), 10, 2 );
-		add_action( 'admin_print_styles', array(__CLASS__, '_style') );
+		add_action( 'admin_print_styles',                              array(__CLASS__, '_style') );
 	}
 
 
-	public static function _add_thumbnail_column( $posts_columns, $post_type ) {
+	public static function _add_thumbnail_column( $posts_columns ) {
 		$new = array( '_thumb' => __('Thumbnail') );
 		$pos = array_search( 'cb', array_keys($posts_columns) );
 
